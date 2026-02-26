1ec117c9a84761a4d49a52280b5984b282650fac
Farmix2003 <ffarrux386@gmail.com>
2026-02-26 08:44:19 +0500

    Initial commit on Data Structures, LinkedList, class creation, add method implementation and print method

---
+++ src/LinkedList.java
@@ -0,0 +1,40 @@
+class Node{
+	int data;
+	Node next;
+	
+	public Node(int data) {
+		this.data =data;
+		next = null;
+	}
+}
+
+
+public class LinkedList {
+   Node headNode = null;
+	
+  
+  public void add(int data) {
+	  Node newNode = new Node(data);
+	  
+	  if(headNode == null) {
+		  headNode = newNode;
+	  }else {
+		  Node currentNode = headNode;
+		  while (currentNode.next != null) {
+              currentNode = currentNode.next;			
+		}
+		  currentNode.next = newNode;
+	  }
+  }
+	
+	public void print() {
+		Node current = headNode;
+		
+		while (current != null) {
+			System.out.print(current.data+" ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList srLinkedList = new LinkedList();
+		srLinkedList.add(2);
+		srLinkedList.add(3);
+		srLinkedList.add(7);
+		
+		srLinkedList.print();
+
+	}
+
+}
