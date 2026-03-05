8e568219ee795dd4f60bef0ffc6648dcfa0648f0
Farmix2003 <ffarrux386@gmail.com>
2026-03-05 19:48:45 +0500

    Implementing methods for removing, reversing, adding at index,

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,49 +1,130 @@
-class Node{
+
+ class Node {
 	int data;
 	Node next;
-	
 	public Node(int data) {
-		this.data =data;
-		next = null;
+		this.data = data;
+		this.next = null;
 	}
 }
 
 
-public class LinkedList {
-   Node headNode = null;
+public class LinkedList{
+
+	private Node head = null;
+	private int size;
 	
-  
-  public void add(int data) {
-	  Node newNode = new Node(data);
-	  
-	  if(headNode == null) {
-		  headNode = newNode;
-	  }else {
-		  Node currentNode = headNode;
-		  while (currentNode.next != null) {
-              currentNode = currentNode.next;			
+	public void add(int data) {
+		// TODO Auto-generated method stub
+		Node newNode = new Node(data);
+		if(head == null) {			
+			head = newNode;
+		}else {
+			Node current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
 		}
-		  currentNode.next = newNode;
-	  }
-  }
+    size++;
+	}
 	
-  public void addFirst(int data) {
-	  Node newNode = new Node(data);
-	  newNode.next = headNode;
-	  headNode = newNode;
-	  
-	  
-  }
-  
-  
+	public void addFirst(int data) {
+		
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+		size++;
+	}
+	
+
+	public void add(int index, int data) {
+		if(index<0 || index>size) throw new IndexOutOfBoundsException();
+		
+		if(index == 0) {add(data); return;}
+		
+		Node newNode = new Node(data);
+		Node currentNode = head;
+		for (int i = 0; i < index-1; i++) {
+			currentNode = currentNode.next;
+		}
+		newNode.next = currentNode.next;
+		currentNode.next = newNode;
+		size++;
+		
+	}
+
 	public void print() {
-		Node current = headNode;
+		// TODO Auto-generated method stub
+		Node current = head;
+		if(head == null) return;
 		
 		while (current != null) {
-			System.out.print(current.data+" ");
+			System.out.print(current.data + " ");
 			current = current.next;
 		}
 		System.out.println();
 	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return size;
+	}
+
+	public void remove(int data) {
+		if(head == null) return;
+		
+		Node currentNode = head;
+		  if (head.data == data) {
+	            head = head.next;
+	            size--;
+	            return;
+	        }
+		while (currentNode.next != null && currentNode.next.data != data) {
+			currentNode = currentNode.next;
+		}
+		if(currentNode.next != null) {
+			currentNode.next = currentNode.next.next;
+		}
+		size--;
+		
+	}
+
+	public int get(int index) {
+		if(index<0 || index>size) throw new IndexOutOfBoundsException();
+		Node currentNode = head;
+		
+		for (int i = 0; i < index; i++) {
+			currentNode = currentNode.next;
+		}
+		
+		return currentNode.data;
+	}
+	
+	public Node getNode(int index) {
+	    if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
+	    Node current = head;
+	    for(int i = 0; i < index; i++) {
+	        current = current.next;
+	    }
+	    return current;
+	}
+
+	public void reverse() {
+		// TODO Auto-generated method stub
+		Node prevNode = null;
+		Node currentNode = head;
+		Node nextNode = null;
+		
+		while (currentNode != null) {
+			nextNode = currentNode.next;
+			currentNode.next = prevNode;
+			prevNode = currentNode;
+			currentNode = nextNode;
+		}
+		head = prevNode;
+	}
+
+
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
-
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		List<String> nLinkedList = new java.util.LinkedList<>();
+		
+		
 		LinkedList srLinkedList = new LinkedList();
 		srLinkedList.add(2);
 		srLinkedList.add(3);
 		srLinkedList.add(7);
-		
+	
 		
 		srLinkedList.addFirst(6);
 		
+		srLinkedList.add(2, 4);
+		
+		srLinkedList.remove(7);
+		
 		srLinkedList.print();
+		
+		srLinkedList.reverse();
+		
+		srLinkedList.print();
+		System.out.println(srLinkedList.get(3));
+		System.out.println(srLinkedList.size());
 
 	}
 
